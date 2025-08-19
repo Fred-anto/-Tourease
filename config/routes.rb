8c17cc6a903c3df918a0d790146d0daad42c0b8f
Tiphaine deSaint-Bon <b00756200@essec.edu>
2025-08-19 17:27:08 +0200

    intitate message

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,8 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  # root "posts#index"*
+  resources :challenges, only: [:index, :show] do
+    resources :messages, only: [:new, :create]
+  end
 end
