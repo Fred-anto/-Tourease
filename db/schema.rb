f0975936d1dac06532b9d94b65b103e675556497
Tiphaine deSaint-Bon <b00756200@essec.edu>
2025-08-21 10:45:34 +0200

    mm

--- db/schema.rb
+++ db/schema.rb
@@ -34,9 +34,9 @@ ActiveRecord::Schema[7.1].define(version: 2025_08_20_122554) do
 
   create_table "chats", force: :cascade do |t|
     t.string "title"
-    t.bigint "trip_id"
+    t.bigint "trip_id", null: false
     t.bigint "user_id", null: false
-    t.bigint "activities_id"
+    t.bigint "activities_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["activities_id"], name: "index_chats_on_activities_id"
