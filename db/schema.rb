bad912ce0a74587c04f2455c844359e271fd8f19
Tiphaine deSaint-Bon <b00756200@essec.edu>
2025-08-22 14:35:49 +0200

    display mess dans trip

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_08_22_092104) do
+ActiveRecord::Schema[7.1].define(version: 2025_08_20_181213) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -118,7 +118,6 @@ ActiveRecord::Schema[7.1].define(version: 2025_08_22_092104) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
-    t.text "description"
   end
 
   create_table "users", force: :cascade do |t|
