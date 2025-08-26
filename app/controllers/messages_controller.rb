5fa776582daa1864bb1d906a27388c7733402d6b
Tiphaine deSaint-Bon <b00756200@essec.edu>
2025-08-26 12:44:29 +0200

    mm

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -17,12 +17,22 @@ class MessagesController < ApplicationController
        #{@trip.destination} from #{@trip.start_date} to #{@trip.end_date}.
       Help me plan my #{@trip.mood} trip with daily activities of must-see and trendy spots.
       Each day should be labeled with the day of the week and the date, based on my trip schedule: from #{@trip.start_date} to #{@trip.end_date}.
-      Answer in bullet points, with for each activity:
-      - description - start_time - time allocated - One type of activity among:
-      Culture, Nature, Sport, stroll, Food or Nightlife - and address"
+      Output strictly in JSON format, with no additional text.
+      Provide a list of activities.
+      Very important, your message MUST be a valid JSON
+      Each activity must include the following keys:
+      'Schedule' - set this value to true only when the user ask you to plan for activites
+      'name' - short name of the activity
+      'description' - short description of the activity
+      'start_date_time' - start date and time in the format YYYY-MM-DD HH:MM
+      'end_date_time' - end date and time in the format YYYY-MM-DD HH:MM
+      'category' - choose one and only one among: Culture, Nature, Sport, Relaxation, Food, or Nightlife
+      'address' - location of the activity.
+      'notes' - Fill this key only when the user doesn't ask for an activity with your response"
 
-      response = @chat_message.with_instructions(system_prompt).ask(@message.content)
-      @chat.messages.create(role: "assistant", content: response.content)
+
+      response = @chat_message.with_instructions(system_prompt).ask(@message.content.to_s)
+      @chat.messages.create(role: "assistant", content: response.content, parsed_content: JSON.parse(response.content))
 
       @chat.generate_title_from_first_message if @chat.title == "New Chat"
 
